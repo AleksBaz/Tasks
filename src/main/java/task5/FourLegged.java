2a33b94451ce9e21f55693855e393726eae2367b
AleksBaz <Bazanyonok@gmail.com>
2019-06-03 00:17:24 +0300

    add homework task5 and task6

---
+++ src/main/java/task5/FourLegged.java
@@ -0,0 +1,17 @@
+package task5;
+//question 20
+public class FourLegged {
+
+        String walk = "walk,";
+        static class BabyRhino extends FourLegged {
+            String walk ="toddle,";
+        }
+
+        public static void main(String[] args) {
+            FourLegged f = new BabyRhino();
+            BabyRhino b = new BabyRhino();
+            System.out.println(f.walk);
+            System.out.println(b.walk);
+        }
+
+}
