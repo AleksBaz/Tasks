2ec15df744af0e34ea2d1ee7101c9f8d123505dd
AleksBaz <Bazanyonok@gmail.com>
2019-06-03 00:18:26 +0300

    add homework task5 and task6

---
+++ src/main/java/task6/DoubleOperation.java
@@ -0,0 +1,9 @@
+package task6;
+
+public interface DoubleOperation<T extends Number> {
+    T calculate(T first, T second);
+
+    default String interfaceInfo() {
+        return "This is functional interface for operation on two parameters.";
+    }
+}
---
+++ src/main/java/task6/Main.java
@@ -0,0 +1,9 @@
+package task6;
+
+public class Main {
+    public static void main(String[] args) {
+        DoubleOperation<Double> doubleOperation = (k, v) -> k-v;
+        System.out.println(doubleOperation.calculate(5.5, 1.3));
+        System.out.println(doubleOperation.interfaceInfo());
+    }
+}
