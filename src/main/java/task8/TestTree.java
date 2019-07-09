a1e4bb657abe94af142483bff7f2253fb2a9edc8
AleksBaz <Bazanyonok@gmail.com>
2019-07-10 00:09:52 +0300

    In task8 replace remove by removeIf

--- src/main/java/task8/TestTree.java
+++ src/main/java/task8/TestTree.java
@@ -2,6 +2,11 @@ package task8;
 
 import java.util.TreeSet;
 
+/** Task 8 TreeSet - создать древовидную структуру хранения номеров шаров от боулинга (шаров всего 20)
+ *  (учесть что шары 2 и 7 хранятся в отдельной коллекции - там смещен центр тяжести (эти шары служат для обмана)
+ *  проитерироваться по дереву, удалить шары 3 и 17 из дерева, проитерироваться по дереву
+ */
+
 public class TestTree {
     public static void main(String[] args){
         TestTree balls = new TestTree();
@@ -11,9 +16,7 @@ public class TestTree {
             if (i != 2 && i != 7) {
                 bowlingBalls.add(i);
             }
-            if (i == 3 || i == 17) {
-                bowlingBalls.remove(i);
-            }
+                bowlingBalls.removeIf(a -> a == 3 || a == 17);
         }
         System.out.println(bowlingBalls.toString());
     }
